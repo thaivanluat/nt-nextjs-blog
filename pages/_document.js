c1b486ef803541770730de49ef7bb85e42d5e4c5
Law Thai <dev.law1@martaskify.com>
2025-09-18 14:41:39 +0700

    add more tag for testing

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,14 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8999361861337865"
           crossOrigin="anonymous"
         />
+
+        {/* COAD */}
+        <script
+          async
+          src="https://verify.coad.com/check.js?client=coad-pub-CYdNJkqa5BksOF6HW"
+          crossOrigin="anonymous"
+        />
+        <meta name="coad-publisher-account" content="coad-pub-CYdNJkqa5BksOF6HW" />
       </Head>
       <body>
         <Main />
