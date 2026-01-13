474d4942d3c372db512067596f2fade0bd32145a
Law Thai <dev.law1@martaskify.com>
2026-01-13 21:58:16 +0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,7 @@ export default function Document() {
           crossOrigin="anonymous"
         />
 		<script async src='https://cdn.coad.be3pi.com/js/coad-analytics.js?client=019adf94-5dd6-7160-85de-3fcd975955e2' crossorigin='anonymous'></script>
+		<script async src='https://cdn.coad.be3pi.com/js/coad-analytics.js?client=019bb7d4-b189-7359-b0c3-d450ebf753b3' crossorigin='anonymous'></script>
 		<meta name="coad-publisher-account" content="coad-pub-g7larehvjvAfhf5kG"></meta>
 		<meta name="coad-publisher-account" content="coad-pub-YE5Bf23wGpwgy2OFk"></meta>
 		<meta name="coad-publisher-account" content="coad-pub-YE5Bf23wGpwgy2OFk"></meta>
